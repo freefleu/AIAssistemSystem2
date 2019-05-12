b2709f74ae3677e37215667e763816e164de092b
zhangyouyouyou <1213637889@qq.com>
2019-05-12 15:06:52 +0800

    Add files via upload

--- aiassistantv2.0/assistantmodel/testhtml.py
+++ aiassistantv2.0/assistantmodel/testhtml.py
@@ -1,13 +1,7 @@
 from django.test import TestCase
-#import unittest
-from assistantmodel.models import Student
-from django.test import Client
 
 class SimpleTest(TestCase):
-#class testhtml(unittest.TestCase):
     def test_login(self):
-        response=self.client.get('/login-post')
-        self.assertEqual(response.status_code,200)
+        response = self.client.get('/login-post')
+        self.assertEqual(response.status_code, 200)
 
-'''if __name__=='__main__':
-    unittest.main()'''
